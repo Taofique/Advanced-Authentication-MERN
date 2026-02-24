577fc6a0a1c8d093ffbeb5e417dacbe6faff68c1
Taofique <shornob8@gmail.com>
2026-02-25 03:11:59 +0600

    feat: auth routes, user Model and controller implemented

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "../db/connectDB.js";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello I am Express server running on port");
 });
+
+app.use("/api/auth", authRoutes);
 app.listen(3000, () => {
   connectDB();
   console.log("Server is running on port 3000");
---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: Date.now,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+  },
+
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
